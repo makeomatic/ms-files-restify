97620b225a5bd911e0e05da2981585ba36a85225
Vitaly Aminev <v@aminev.me>
2015-12-20 19:06:54 +0300

    feat(download): adds meta headers
    BREAKING CHANGE: works with ms-files 0.4.x, where new response format is given to download link
    Adds extra headers, which allows to fetch particular bytes of a model to get the desired data

--- src/endpoints/download.js
+++ src/endpoints/download.js
@@ -1,6 +1,7 @@
 const config = require('../config.js');
 const { getRoute, getTimeout } = config;
 const ROUTE_NAME = 'download';
+const base64 = require('urlsafe-base64');
 
 /**
  * @api {get} /download/:filename Download provided file
@@ -30,6 +31,9 @@ const ROUTE_NAME = 'download';
  *
  * @apiSuccessExample {json} Success-Download:
  * 		HTTP/1.1 302 Moved Temporarily
+ * 		X-Content-Preview: 4-xxxx
+ * 		X-Content: yyyy-zzzz
+ * 		X-Content-MD5: base64(md5Hash)
  * 		Location: https://storage.googleapis.com/bucket-name/username/49058df9-983e-43b6-8755-84b92c272357?GoogleAccessId=xxx&expires=231283612781232&signature=xxx
  */
 exports.get = {
@@ -45,9 +49,29 @@ exports.get = {
 
       return req.amqp
         .publishAndWait(getRoute(ROUTE_NAME), message, { timeout: getTimeout(ROUTE_NAME) })
-        .then(signedURL => {
-          res.setHeader('Location', signedURL);
+        .then(({ url, data }) => {
+          const { checksum, previewSize, modelSize } = data;
+
+          if (previewSize && modelSize) {
+            // inclusive bytes, starts on 5th byte, 0 - 1st byte, 3 - 4th byte
+            // lastByte = (4 - 1) + length
+            const previewLastByte = 3 + parseInt(previewSize, 10);
+            // start right after preview
+            const modelStart = previewLastByte + 1;
+            // preview + length - 1
+            const modelStop = modelStart + parseInt(previewSize, 10) - 1;
+
+            res.setHeader('X-Content-Preview', `bytes=4-${previewLastByte}`);
+            res.setHeader('X-Content', `bytes=${modelStart}-${modelStop}`);
+          }
+
+          if (checksum) {
+            res.setHeader('X-Content-MD5', base64.encode(new Buffer(data.checksum, 'hex')));
+          }
+
+          res.setHeader('Location', url);
           res.send(302);
+          return false;
         })
         .asCallback(next);
     },
