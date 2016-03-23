2079ab793661abaff084e30370a817258fb3028a
Ivan <Violentov@MacBook-Air-Ivan.local>
2016-03-23 13:42:28 +0300

    Fix update endpoint

--- src/endpoints/update.js
+++ src/endpoints/update.js
@@ -1,23 +1,20 @@
 const validator = require('../validator.js');
 const config = require('../config.js');
-const get = require('lodash/get');
 const { getRoute, getTimeout } = config;
 const ROUTE_NAME = 'update';
 
-exports.post = {
-  path: '/update/:uploadId',
+exports.patch = {
+  path: '/update',
   middleware: ['auth'],
   handlers: {
     '1.0.0': function updateFileInformation(req, res, next) {
       return validator
         .validate(ROUTE_NAME, req.body)
         .then(body => {
-          const { amqp } = req;
-          const { uploadId } = req.params;
-          const { meta } = body.data;
-          const isAdmin = req.user.isAdmin();
-          const message = { meta, uploadId, isAdmin };
-          const username = get(req, 'user.id');
+          const { amqp, user } = req;
+          const attributes = body.data.attributes;
+          const message = { ...attributes };
+          const username = user.id;
 
           if (username) {
             message.username = username;
@@ -30,11 +27,7 @@ exports.post = {
           );
         })
         .then(result => {
-          res.send(201, {
-            type: 'update',
-            id: result.uploadId,
-            result
-          });
+          res.send(204);
           return false;
         })
         .asCallback(next);
