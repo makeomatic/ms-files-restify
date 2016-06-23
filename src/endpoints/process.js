d9b40d34e538f46df3923d97fd60f71f25f244bf
Vitaly Aminev <v@aminev.me>
2016-06-23 12:44:44 +0300

    draft: export function

---
+++ src/endpoints/process.js
@@ -0,0 +1,61 @@
+const config = require('../config.js');
+const { getRoute, getTimeout } = config;
+const ROUTE_NAME = 'process';
+
+/**
+ * @api {post} /process/:filename Re-processes filename based on input metadata
+ * @apiVersion 1.0.0
+ * @apiName ProcessFile
+ * @apiGroup Files
+ * @apiPermission none
+ *
+ * @apiDescription Returns 201 Accepted, poll to get results of processing
+ *
+ * @apiParam (Params) {String} filename
+ *
+ * @apiExample {curl} Example usage:
+ *   curl -H 'Accept: application/vnd.api+json' \
+ *   	 -X POST -d '{
+ *   	   "data": {
+ *   	   	 "type": "file",
+ *   	   	 "id": "9058df9-983e-43b6-8755-84b92c272357",
+ *   	   	 "attributes": {
+ *   	   	   "export": {
+ *   	   	     "type": "wrl",
+ *   	   	     "meta": {
+ *   	   	       // some extra flags / meta
+ *   	   	     }
+ *   	   	   }
+ *   	   	 }
+ *   	   }
+ *   	 }' \
+ *     "https://api-sandbox-dev.matic.ninja/api/files/process"
+ *
+ * @apiUse FileNotFoundError
+ * @apiUse PreconditionFailedError
+ *
+ * @apiSuccessExample {json} Success-Download:
+ *     HTTP/1.1 201 Accepted
+ */
+exports.get = {
+  path: '/process',
+  middleware: ['auth'],
+  handlers: {
+    '1.0.0': function getDownloadURL(req, res, next) {
+      // basic message
+      const message = {
+        filename: req.body.data.id,
+        params: req.body.data.attributes,
+        username: req.user.id,
+      };
+
+      return req.amqp
+        .publishAndWait(getRoute(ROUTE_NAME), message, { timeout: getTimeout(ROUTE_NAME) })
+        .then(() => {
+          res.send(201);
+          return false;
+        })
+        .asCallback(next);
+    },
+  },
+};
