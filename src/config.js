3caf760b8c62ed827ce1b45f4f8d4678fbb974a6
AVVS <v@makeomatic.ru>
2017-04-23 22:08:54 -0700

    feat: gce push handler

--- src/config.js
+++ src/config.js
@@ -28,6 +28,7 @@ const config = module.exports = {
       token: '',
       channel: '',
     },
+    gce_v2: [],
   },
 };
 
--- src/middleware/gce_v2.js
+++ src/middleware/gce_v2.js
@@ -1,13 +1,48 @@
 /**
  * more docs available: https://cloud.google.com/storage/docs/object-change-notification#_Type_Sync
  */
+const Promise = require('bluebird');
+const { HttpStatusError } = require('common-errors');
+const find = require('lodash/find');
+const config = require('../config.js');
+
+// bucket configuration
+const $gce = config.files.gce_v2;
+const gce = Array.isArray($gce) ? $gce : [$gce];
+
+function parseInput(req) {
+  const message = req.body.message;
+  const { bucket, eventType, resource } = message.attributes;
+  const gceSettings = find(gce, { bucket });
+
+  if (!gceSettings) {
+    return Promise.reject(new HttpStatusError(400, 'failed to find gce bucket'));
+  }
+
+  if (gceSettings.token !== req.query.token) {
+    return Promise.reject(new HttpStatusError(400, 'token mismatch'));
+  }
+
+  let action;
+  switch (eventType) {
+    case 'OBJECT_FINALIZE':
+      action = 'exists';
+      break;
+
+    default:
+      return Promise.reject(new HttpStatusError(400, 'unsupported action'));
+  }
+
+  req.file = {
+    action,
+    resourceId: resource,
+    filename: JSON.parse(Buffer.from(message.data)).name,
+  };
+  return null;
+}
 
 module.exports = function gceWebhook(req, res, next) {
-  // const headers = req.headers;
-  // const $gce = config.files.gce_v2;
-  // const gce = Array.isArray($gce) ? $gce : [$gce];
-
-  req.log.info({ body: req.body, headers: req.headers }, 'push notification from google cloud v2');
-
-  return next(new Error('not implemented'));
+  return Promise
+    .try(parseInput.bind(req))
+    .asCallback(next);
 };
